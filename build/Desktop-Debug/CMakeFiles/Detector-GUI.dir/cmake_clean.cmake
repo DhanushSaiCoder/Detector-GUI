25b438b4061bcf5c7495f5050a85867d2fec2af9
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-09-30 21:45:22 +0530

    Intitial form QT

---
+++ build/Desktop-Debug/CMakeFiles/Detector-GUI.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Detector-GUI.dir/main.cpp.o"
+  "CMakeFiles/Detector-GUI.dir/main.cpp.o.d"
+  "Detector-GUI"
+  "Detector-GUI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Detector-GUI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
